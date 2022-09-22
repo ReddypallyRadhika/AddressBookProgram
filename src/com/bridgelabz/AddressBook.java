3e33dedc2f245f16d93bc26904e50f47441d3f6d
Radhika <reddypallyradhika@gmail.com>
2022-09-22 15:24:49 +0530

    Refactored the code by adding comment

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -6,11 +6,9 @@ package com.bridgelabz;
 /**
  * @author hp
  *
- *
- *Ability to create a Contacts in Address
-Book with first and last names, address,
-city, state, zip, phone number and
-email...
+ *UC1
+ *Ability to create a Contacts in Address Book with first and last names, address,
+ *city, state, zip, phone number and email...
  *
  *
  *
